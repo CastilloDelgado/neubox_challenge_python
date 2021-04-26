609e087061c41e87bb0b831b6a363af161bb8cb4
CastilloDelgado <CastilloDelgado@users.noreply.github.com>
2021-04-26 12:55:52 -0500

    Correcciones de prueba 1
    Se hacen las correcciones sugeridas en la prueba 1

--- problema1.py
+++ problema1.py
@@ -1,25 +1,56 @@
 import re
+import os
 
-# Leemos el archivo txt
-inputFile = open("input_problema1.txt", "r")
+clear = lambda: os.system('cls')
+clear()
+exit = 1
 
-# Tomar los varoles por linea y eliminar caracterelcs especiales
-values = inputFile.readline().split()
-# Eliminamos caracteres especiales del texto
-m1 = re.sub('[^A-Za-z0-9]+', '', inputFile.readline())
-m2 = re.sub('[^A-Za-z0-9]+', '', inputFile.readline())
-message = inputFile.readline()
-cleanMessage = ""
+while exit == 1:
+    # Solicitamos el
+    fileName = input("Ingresa el nombre del archivo de texto de entrada sin la extención:\n** El archivo debe de estar en la misma carpeta que el programa **\n") + ".txt"
+    inputFile = open(fileName, "r")
+    content = inputFile.read()
 
-# Limbiar duplicados del string
-for char in message:
-    if len(cleanMessage) == 0:
-        cleanMessage = char
+    lines = []
+    for line in content.split("\n"):
+        lines.append(re.sub('[^A-Za-z0-9 ]+', '', line))
 
-    elif len(cleanMessage) > 0 and cleanMessage[len(cleanMessage)-1] != char:
-        cleanMessage += char
+    errors = ""
+    # Validar datos del archivo
+    values = lines[0].split(" ")
+    
+    errors += "El valor en M1 no esta entre 2 y 50\n" if int(values[0]) < 2 or int(values[0]) > 50 else ""
+    errors += "El valor en M2 no esta entre 2 y 50\n" if int(values[1]) < 2 or int(values[1]) > 50 else ""
+    errors += "El valor en N no esta entre 3 y 5000\n" if int(values[2]) < 3 or int(values[2]) > 5000 else ""
+    errors += "El valor en M1 no corresponde al tamanio de la primera instruccion\n" if (int(values[0]) != len(lines[1])) else ""
+    errors += "El valor en M2 no corresponde al tamanio de la segunda instruccion\n" if int(values[1]) != len(lines[2]) else ""
+    errors += "El valor en N no corresponde al tamanio del mensaje \n" if int(values[2]) != len(lines[3]) else ""
 
-# Crear archivo output
-outputFile = open("output.txt", "w+")
-outputFile.write(("SI\n" if cleanMessage.find(m1) >= 0 else "NO\n") + ("SI" if cleanMessage.find(m2) >= 0 else "NO"))
-outputFile.close()
+    if errors != "":
+        # Si hay errores, los imprimimos y reiniciamos el código
+        print(errors)
+    else:
+        # Tomar los varoles por linea y eliminar caracterelcs especiales
+        # Eliminamos caracteres especiales del texto
+        message = lines[3]
+        cleanMessage = ""
+
+        # Limpiar duplicados del string
+        for char in message:
+            if len(cleanMessage) == 0:
+                cleanMessage = char
+
+            elif len(cleanMessage) > 0 and cleanMessage[len(cleanMessage)-1] != char:
+                cleanMessage += char
+
+        # Crear archivo output
+        outputFile = open("output.txt", "w+")
+        outputFile.write(("Si\n" if cleanMessage.find(lines[1]) >= 0 else "No\n") + ("Si" if cleanMessage.find(lines[2]) >= 0 else "No"))
+        outputFile.close()
+
+        print("Archivo Output creado con exito!\n")
+
+    exit = int(input("¿Desea ingresar otro archivo? Ingrese una opción: \n- 0 = No\n- 1 = Si\nIngrese la opción deseada: "))
+    clear()
+
+print("Adiós!")
--- problema2.py
+++ problema2.py
@@ -1,28 +1,64 @@
 import re
+import os
 
-# Leemos el archivo txt
-inputFile = open("input_problema2.txt", "r")
+clear = lambda: os.system('cls')
+clear()
+exit = 1
 
-# Tomamos la cantidad de rondas e iteramos por las lineas del archivo
-winner = ""
-bestScore = 0
+while exit == 1:
 
-for line in range(int(inputFile.readline())):
-    # Convertir valores de cada ronda a enteros
-    values = inputFile.readline().split(" ")
-    values[0] = int(values[0])
-    values[1] = int(values[1])
-    
-    # Calculamos el score de cada partida (siempre positivo)
-    score = values[0] - values[1] if values[0] - values[1] > 0 else (values[0] - values[1]) * -1
+    # Leemos el archivo txt
+    fileName = input("Ingresa el nombre del archivo de texto de entrada sin la extención:\n** El archivo debe de estar en la misma carpeta que el programa **\n") + ".txt"
+    inputFile = open(fileName, "r")
+    content = inputFile.read()
+    lines = []
+        
+    # Validamos la informacin del archivo
+    try: 
+        for line in content.split("\n"):
+            values = [] 
+            for value in line.split(" "):
+                values.append(int(value))
+            lines.append(values)
+    except:
+        print("\nHay valores que no son enteros en el archivo\n")
+        exit = int(input("¿Desea ingresar otro archivo? Ingrese una opción: \n- 0 = No\n- 1 = Si\nIngrese la opción deseada: "))
+        clear()
 
-    # Comparamos el score de la partida con el mejor score registrado
-    if score > bestScore:
-        bestScore = score    
-        winner = "1" if values[0] - values[1] > 0 else "2"
+    errors = ""
 
-# Crear archivo output
-outputFile = open("output.txt", "w+")
-outputFile.write(str(winner) + " " + str(bestScore))
-outputFile.close()
+    errors += "El valor en la linea 1 no corresponde al total de rondas capturadas en el archivo\n" if lines[0][0] != (len(lines)-1) else ""
+
+    if errors != "":
+        print(errors)
+    else:
+
+    # Tomamos la cantidad de rondas e iteramos por las lineas del archivo
+        winner = ""
+        bestScore = 0
+
+        for index in range(lines[0][0]):
+            # Convertir valores de cada ronda a enteros
+            if index != 0:
+                values = lines[index]
+                
+            # Calculamos el score de cada partida (siempre positivo)
+            score = values[0] - values[1] if values[0] - values[1] > 0 else (values[0] - values[1]) * -1
+
+            # Comparamos el score de la partida con el mejor score registrado
+            if score > bestScore:
+                bestScore = score    
+                winner = "1" if values[0] - values[1] > 0 else "2"
+
+        # Crear archivo output
+        outputFile = open("output.txt", "w+")
+        outputFile.write(str(winner) + " " + str(bestScore))
+        outputFile.close()
+
+        print("Archivo Output creado con exito!\n")
+            
+    exit = int(input("¿Desea ingresar otro archivo? Ingrese una opción: \n- 0 = No\n- 1 = Si\nIngrese la opción deseada: "))
+    clear()
+
+print("Adiós!")
 
